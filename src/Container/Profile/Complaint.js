e132be42075f6588f7d30474c7accac3d46de0d9
Nishant kumar Gupta <nishantg1210@gmail.com>
2021-08-02 22:40:55 +0530

    feat:mobile view ui changes

--- src/Container/Profile/Complaint.js
+++ src/Container/Profile/Complaint.js
@@ -6,59 +6,61 @@ export class Complaint extends Component {
         return (
             <div>
                 <p className='complaintshead' style={{ marginTop: '2%' }}>My Complaints</p>
-                <div className="tble_box1complaint">
-                    <table className="col_names1">
-                        <tbody>
-                        <tr className="table_headingnew">
-                            <td className="product_imagecomplaint">S.No</td>
-                            <td className="product_Namecomplaint">
-                            <span className="large_screen_text_425">Complaint Heading</span>
-                            <span className="small_screen_text_425">Complaint</span>
-                            </td>
-                            <td className="product_availnewcomplaint">Complaint ID</td>
-                            <td className="product_codenewcomplaint">Status</td>
-                            <td className="product_pricenewcomplaint">Replied</td>
-                        </tr>
-                        <tr className="table_heading2">
-                            <td className="product_image1complaint">1</td>
-                            <td className="product_Name1complaint">
-                                My LG washing machine is not working properly
-                            </td>
-                            <td className="product_avail1newcomplaint">8273791MG</td>
-                            <td className="product_code1complaint">Sent</td>
-                            <td className="product_price1newcomplaint">
-                                <div className="save_box">
-                                    <select className="dropdown_colors1" >
-                                        <option value="WH">Yes</option>
-                                        <option value="BL">No</option>
-                                    </select>
-                                </div>
-                            </td>
-                            <td className="product_quantity1newcomplaint">
-                                View Details
-                            </td>
-                        </tr>
-                        <tr className="table_heading2">
-                            <td className="product_image1complaint">1</td>
-                            <td className="product_Name1complaint">
-                                My LG washing machine is not working properly
-                            </td>
-                            <td className="product_avail1newcomplaint">8273791MG</td>
-                            <td className="product_code1complaint">Sent</td>
-                            <td className="product_price1newcomplaint">
-                                <div className="save_box">
-                                    <select className="dropdown_colors1" >
-                                        <option value="WH">Yes</option>
-                                        <option value="BL">No</option>
-                                    </select>
-                                </div>
-                            </td>
-                            <td className="product_quantity1newcomplaint">
-                                View Details
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+                <div className="scroll_x_box">
+                    <div className="tble_box1complaint">
+                        <table className="col_names1">
+                            <tbody>
+                                <tr className="table_headingnew">
+                                    <td className="product_imagecomplaint">S.No</td>
+                                    <td className="product_Namecomplaint">
+                                        <span className="large_screen_text_425">Complaint Heading</span>
+                                        <span className="small_screen_text_425">Complaint</span>
+                                    </td>
+                                    <td className="product_availnewcomplaint">Complaint ID</td>
+                                    <td className="product_codenewcomplaint">Status</td>
+                                    <td className="product_pricenewcomplaint">Replied</td>
+                                </tr>
+                                <tr className="table_heading2">
+                                    <td className="product_image1complaint">1</td>
+                                    <td className="product_Name1complaint">
+                                        My LG washing machine is not working properly
+                                    </td>
+                                    <td className="product_avail1newcomplaint">8273791MG</td>
+                                    <td className="product_code1complaint">Sent</td>
+                                    <td className="product_price1newcomplaint">
+                                        <div className="save_box">
+                                            <select className="dropdown_colors1" >
+                                                <option value="WH">Yes</option>
+                                                <option value="BL">No</option>
+                                            </select>
+                                        </div>
+                                    </td>
+                                    <td className="product_quantity1newcomplaint">
+                                        View Details
+                                    </td>
+                                </tr>
+                                <tr className="table_heading2">
+                                    <td className="product_image1complaint">1</td>
+                                    <td className="product_Name1complaint">
+                                        My LG washing machine is not working properly
+                                    </td>
+                                    <td className="product_avail1newcomplaint">8273791MG</td>
+                                    <td className="product_code1complaint">Sent</td>
+                                    <td className="product_price1newcomplaint">
+                                        <div className="save_box">
+                                            <select className="dropdown_colors1" >
+                                                <option value="WH">Yes</option>
+                                                <option value="BL">No</option>
+                                            </select>
+                                        </div>
+                                    </td>
+                                    <td className="product_quantity1newcomplaint">
+                                        View Details
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
                 <ComplaintPop />
                 <p className='mailId'>Write to us at lokostop1@gmail.com</p>
